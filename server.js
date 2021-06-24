fa5dbb949ed540984ec9a88617e38c3b15fa98ce
LeninRivera <leninra24@gmail.com>
2021-06-24 19:45:44 -0400

    Connect mongoose with express

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
 const app = require("./server/app"),
-  port = 8080;
+  port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Express server is up on port ${port}`));
--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require("./db/config");
 const express = require("express"),
   app = express();
 
