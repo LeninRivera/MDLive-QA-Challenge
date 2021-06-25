7db099145df739f7dc5a4faafd6ac351bb1cae3f
LeninRivera <leninra24@gmail.com>
2021-06-24 21:31:49 -0400

    Add test endpoint and fix express.json middleware

--- server/app.js
+++ server/app.js
@@ -3,6 +3,12 @@ const express = require("express"),
   app = express();
 
 //Parse incoming JSON into objects
-app.use(express.json);
+app.use(express.json());
+
+//todo delete before final commit
+app.get("/test", (req, res) => {
+  console.log("test point is working");
+  res.status(200).send("Test point is working");
+});
 
 module.exports = app;
