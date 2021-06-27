2ad11e690a7b5748835c74af630dd65eb29c8f0c
LeninRivera <leninra24@gmail.com>
2021-06-27 01:08:32 -0400

    Add route endpoints missing logic for by: name

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 require("./db/config");
 const express = require("express"),
-  app = express();
+  app = express(),
+  appsRoute = require("./routes/apps");
 
 //Parse incoming JSON into objects
 app.use(express.json());
+
+app.use("/apps", appsRoute);
 
 //todo delete before final commit
 app.get("/test", (req, res) => {
